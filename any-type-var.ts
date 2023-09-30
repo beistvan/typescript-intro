64490f8ee6f6331eba51bc17f2f0f4b52e3906a9
Beregszászi István <beistvan@gmail.com>
2023-09-30 21:53:08 +0200

    Any Type Of Variable

---
+++ any-type-var.ts
@@ -0,0 +1,13 @@
+let guess;
+
+console.log(`${typeof guess} - the type of guess`); // undefined
+
+guess = "blue";
+
+console.log(`${guess} - is the guess`);
+
+guess = 42;
+
+console.log(`${guess} - is the guess now`);
+
+console.log(`${typeof guess} - the type of guess`); // number
