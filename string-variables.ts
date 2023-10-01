9d02fa1e74f073a48d121c615383aae7d7d820e3
Beregszászi István <beistvan@gmail.com>
2023-10-01 18:25:32 +0200

    String Variables

---
+++ string-variables.ts
@@ -0,0 +1,6 @@
+let firstName: string = "Jane";
+let title: string = `frontend developer`;
+let profile: string = `My name is ${firstName}. 
+I am a ${title}.`; // multiline string with string interpollation
+
+console.log(profile);
