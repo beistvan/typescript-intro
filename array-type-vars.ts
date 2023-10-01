db275ef14c788ee2aaa53b7cc562410d24ecfbc2
Beregszászi István <beistvan@gmail.com>
2023-10-01 17:48:22 +0200

    Array Type Variables

---
+++ array-type-vars.ts
@@ -0,0 +1,11 @@
+let arrayName: number[];
+
+arrayName = [10, 9];
+arrayName.push(12);
+arrayName.unshift(7);
+
+console.log(arrayName); // [7, 10, 9, 12] 
+
+let names: string[] = ['John', 'Paul', 'Steven', 'Mary', 'Jenna'];
+
+console.log(names);
