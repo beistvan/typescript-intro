ac5c07fd2567334d1724dc7ee56adda70066c7b7
Beregszászi István <beistvan@gmail.com>
2023-10-01 17:38:25 +0200

    Primitive Types In TypeScript

---
+++ primitive-types.ts
@@ -0,0 +1,18 @@
+var varNameNum: number;
+
+var varNameStr: string = "value";
+
+let variableNameBool: boolean = true;
+
+const constantNameNum: number = 12.34;
+
+let counter: number;
+
+varNameNum = 1;
+counter = 2;
+
+console.log(varNameNum);
+console.log(varNameStr);
+console.log(variableNameBool);
+console.log(constantNameNum);
+console.log(counter);
