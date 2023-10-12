dafc11adc305c9a51111852a7ddab9197ac83487
Beregszászi István <beistvan@gmail.com>
2023-10-12 08:36:09 +0200

    Empty Object

---
+++ empty-object.ts
@@ -0,0 +1,7 @@
+let vacant_empty: {};
+// vacant.firstName = 'John'; // error
+
+console.log(vacant_empty); // undefined
+
+let vacant: {} = {};
+console.log(vacant.toString()); // "[object Object]" 
